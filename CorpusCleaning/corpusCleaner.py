975424a42a8607f591b315331179447782e31817
Rodrigo <lastrodrigo@gmail.com>
2018-10-05 17:36:33 -0300

    corpusCleaner working ok

--- CorpusCleaning/corpusCleaner.py
+++ CorpusCleaning/corpusCleaner.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import unicodedata as ud
 import random
+import math
 
 latin_letters = {}
 
@@ -18,7 +19,7 @@ def only_roman_chars(unistr):
                for uchr in unistr
                if uchr.isalpha())
 
-def  word_count(str,counts):
+def  word_count(word,counts):
     """Adds 1 to the occurrences of word in str in the dictionary counts"""
 
     if word in counts:
@@ -27,6 +28,25 @@ def  word_count(str,counts):
         counts[word] = 1
     return counts
 
+def writeShuffledFile(tokens,tokensPerFile,data,directory,prefix,lineCount,vocab):
+    fileCount = 0
+    totalTokenCount = 0
+    
+    while totalTokenCount < tokens:
+        fileCount += 1
+        fileName = os.path.join(directory,prefix+'.'+str(fileCount)+'.clean.shuffled')
+        tokenCount = 0
+        with open(fileName,'w+',encoding='utf8') as f:
+            print('Processsing output %s' % fileName)
+            while tokenCount < tokensPerFile and totalTokenCount < tokens:
+                line = data[lineCount][1]
+                f.write(line+'\n')
+                lineCount += 1
+                tokenCount += len(line.split())
+                for word in line.split():
+                    vocab = word_count(word,vocab) 
+                totalTokenCount += len(line.split())
+    return totalTokenCount,lineCount,vocab
 
 parser = argparse.ArgumentParser(description='Clean corpus')
 parser.add_argument('path',metavar='Path to corpus',type=str, nargs=1)
@@ -58,10 +78,10 @@ outdir = 'out'
 if not os.path.exists(outdir) or not os.path.isdir(outdir):
     os.makedirs(outdir)
 
-if not os.path.exists(os.path.join(outdir,'training')) or not os.pathisdir(os.path.join(outdir,'training')):
+if not os.path.exists(os.path.join(outdir,'training')) or not os.path.isdir(os.path.join(outdir,'training')):
     os.makedirs(os.path.join(outdir,'training'))
 
-if not os.path.exists(os.path.join(outdir,'testing')) or not os.pathisdir(os.path.join(outdir,'testing')):
+if not os.path.exists(os.path.join(outdir,'testing')) or not os.path.isdir(os.path.join(outdir,'testing')):
     os.makedirs(os.path.join(outdir,'testing'))
 
 fileCount = 0
@@ -134,22 +154,3 @@ with open(os.path.join(outdir,'vocab.txt'),'w+',encoding='utf8') as f:
         f.write(x[0] +"\n")
 
 
-def writeShuffledFile(tokens,tokensPerFile,data,directory,prefix,lineCount,vocab):
-    fileCount = 0
-    totalTokenCount = 0
-    
-    while totalTokenCount < tokens:
-        fileCount += 1
-        fileName = os.path.join(directory,prefix+'.'+str(fileCount)+'.clean.shuffled')
-        tokenCount = 0
-        with open(fileName,'w+',encoding='utf8') as f:
-            print('Processsing output %s' % fileName)
-            while tokenCount < tokensPerFile:
-                line = x in data[lineCount] as _,x
-                f.write(line)
-                lineCount += 1
-                tokenCount += len(line.split())
-                for word in line.split():
-                    word_count(word,vocab) 
-        totalTokenCount += tokenCount
-    return totalTokenCount,lineCount,vocab
