41d24cae0f5a8bfb9eac15bc8fcd31580007df7d
Rodrigo Lastra <lastrodrigo@gmail.com>
2018-10-13 19:23:07 -0300

    changed default minOCcurrence to 8

--- CorpusCleaning/corpusCleaner.py
+++ CorpusCleaning/corpusCleaner.py
@@ -61,7 +61,7 @@ parser.add_argument('--testTokens',default=200000000)
 parser.add_argument('--splitFiles',default=100)
 parser.add_argument('--trainPrefix',default='trainingCorpus')
 parser.add_argument('--testPrefix',default='testingCorpus')
-parser.add_argument('--minOccurrence',default=6)
+parser.add_argument('--minOccurrence',default=8)
 parser.add_argument('--keepNonLatin', action="store_true")
 args = parser.parse_args()
 
